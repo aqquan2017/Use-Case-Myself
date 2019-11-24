24ae408b16a8e2bb3dde8ef47c3c51682b2047e6
aqquan2017 <41032252+aqquan2017@users.noreply.github.com>
2019-11-24 23:58:22 +0700

    UC3 completed

--- N5_WatchMenu/Form1.Designer.cs
+++ N5_WatchMenu/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(81, 137);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(545, 247);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Menu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(704, 455);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ N5_WatchMenu/Form1TT.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace N5_WatchMenu
+{
+    partial class Form1
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-2J4GSB2;Initial Catalog=LAQUAN;Integrated Security=True");
+        void connect()
+        {
+            con.Open();
+
+            string sql = "Select * from Stuff ";
+
+            // thực thi lệnh trong csdl
+            SqlCommand comm = new SqlCommand(sql, con);
+            comm.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Button b = new Button() { Height = 100, Width = 100 };
+                b.Text = dr[0].ToString() + dr[1].ToString();
+                flowLayoutPanel1.Controls.Add(b);
+            }
+            //bảng hiển thị dữ liệu trong giao diện
+        }
+
+        
+    }
+}
